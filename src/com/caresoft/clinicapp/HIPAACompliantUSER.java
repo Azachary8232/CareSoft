cc2b9266ade738f26f5fac4ee52ee68f3b691803
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-12-01 18:51:58 -0700

    Updated Files

--- src/com/caresoft/clinicapp/HIPAACompliantUSER.java
+++ src/com/caresoft/clinicapp/HIPAACompliantUSER.java
@@ -1,5 +1,8 @@
 package com.caresoft.clinicapp;
 
-public interface HIPAACompliantUSER {
+public interface HIPAACompliantUSER {	
+	    abstract boolean assignPin(int pin);
+	    abstract boolean accessAuthorized(Integer confirmedAuthID);
+	
 
 }
